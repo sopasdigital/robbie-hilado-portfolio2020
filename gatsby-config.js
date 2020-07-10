ef0550f79d07e95febccb5df8cfbe95884ff7507
sopasdigital <rob.hilado@gmail.com>
2020-07-10 17:46:21 +0100

    Updated plugins to fix FOUC

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,8 +36,17 @@ module.exports = {
         showSpinner: true,
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-plugin-styled-components`,
+      options: {
+        // Add any options here
+      },
+    },
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
   ],
 }
---
+++ src/utils/typography.js
@@ -0,0 +1,11 @@
+import Typography from "typography"
+
+const typography = new Typography({
+  baseFontSize: "18px",
+  baseLineHeight: 1.666,
+  googleFonts: [{ name: "Nanum Gothic" }, { name: "DM Serif Display" }],
+  headerFontFamily: ["Muli", "sans-serif"],
+  bodyFontFamily: ["Muli", "serif"],
+})
+
+export default typography
