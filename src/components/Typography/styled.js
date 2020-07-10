cecbcdf98b5c9c6ba692c2d61ababda5adf9c876
sopasdigital <rob.hilado@gmail.com>
2020-07-10 15:53:27 +0100

    Added about me page

---
+++ src/components/Typography/styled.js
@@ -0,0 +1,59 @@
+import styled from "styled-components"
+
+const Display = styled.h1`
+  font-family: ${({ theme }) => theme.fonts.serif};
+  font-size: 54px;
+  line-height: 54px;
+  letter-spacing: 2px;
+  margin: 30px 0 0;
+
+  color: ${({ theme }) => theme.colors.black};
+`
+
+const CategoryHeading = styled.h6`
+  position: relative;
+  left: 40px;
+  display: inline-block;
+
+  margin-bottom: 10px;
+
+  font-family: ${({ theme }) => theme.fonts.serif};
+  font-size: 16px;
+  line-height: 18px;
+  letter-spacing: 1px;
+
+  color: #666;
+
+  &:after {
+    position: absolute;
+    top: 50%;
+    left: -50px;
+
+    height: 1px;
+    width: 40px;
+
+    content: "";
+
+    border-bottom: 1px solid #ddd;
+    transform: translateY(-50%);
+  }
+`
+
+const Title = styled.h4`
+  padding-bottom: 3px;
+  margin-bottom: 20px;
+
+  font-family: ${({ theme }) => theme.fonts.serif};
+  font-size: 24px;
+  line-height: 28px;
+  letter-spacing: 1px;
+
+  color: #666;
+  border-bottom: 1px solid #ddd;
+`
+
+const Text = styled.p`
+  line-height: 22px;
+`
+
+export { CategoryHeading, Display, Text, Title }
---
+++ src/pages/about.js
@@ -0,0 +1,142 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import { CategoryHeading, Display, Text, Title } from "../components/Typography"
+
+const AboutPage = () => (
+  <Layout bgtext="About">
+    <SEO title="About" />
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
+      <div
+        style={{
+          minHeight: "85vh",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+        }}
+      >
+        <CategoryHeading>About</CategoryHeading>
+        <Display>Robbie Hilado</Display>
+        <span
+          style={{
+            fontSize: "15px",
+            textTransform: "uppercase",
+            letterSpacing: ".7px",
+            marginBottom: "30px",
+            display: "block",
+            color: "#555",
+          }}
+        >
+          Interaction Designer · Front-end Developer · Calligraphy Enthusiast
+        </span>
+        <div style={{ maxWidth: "650px" }}>
+          <Text>
+            I am an Interaction Designer and Front-end Developer with more than
+            5 years commercial experience working with clients through digital
+            media companies as well as freelance work.
+          </Text>
+          <Text>
+            I come from a background of Design and Arts but have always loved
+            development and implementation. It is this combination that I
+            believe sets me apart from a lot of other designers or developers
+            that stay clear of the other side of the coin. My experience in both
+            design and development have greatly impacted how I see, interact,
+            implement and improve my work and projects I'm working on at both
+            ends of the spectrum.
+          </Text>
+        </div>
+      </div>
+      <div style={{ display: "flex" }}>
+        <div style={{ width: "30%", paddingRight: "50px" }}>
+          <div style={{ marginBottom: "50px" }}>
+            <Title>Core Strengths</Title>
+            <Text>Interaction Design</Text>
+            <Text>Front End Development</Text>
+            <Text>User Experience Design</Text>
+            <Text>Calligraphy &amp; Lettering</Text>
+            <Text>Sprint Facilitation (Sprint 2.0)</Text>
+            <Text>Illustration</Text>
+          </div>
+          <div style={{ marginBottom: "50px" }}>
+            <Title>Tools</Title>
+            <Text>Git</Text>
+            <Text>VS Code / Sublime Text</Text>
+            <Text>Sketch / Figma</Text>
+            <Text>Adobe Illustrator / Photoshop</Text>
+            <Text>Adobe Lightroom</Text>
+            <Text>MacOS / Windows</Text>
+            <Text>Design Sprint 2.0</Text>
+            <Text>Lightning Decision Jam</Text>
+            <Text>Post-its</Text>
+            <Text>Good ol' Pen &amp; Paper</Text>
+            <Text>Various Art Materials</Text>
+          </div>
+          <div style={{ marginBottom: "50px" }}>
+            <Title>Technologies</Title>
+            <Text>Javascript</Text>
+            <Text>React (CRA / Gatsby)</Text>
+            <Text> HTML5 + CSS3</Text>
+          </div>
+          <div>
+            <Title>Curious About</Title>
+            <Text>Advanced CSS and SVG Animation</Text>
+            <Text>Motion Design</Text>
+            <Text>Typography &amp; Font Development</Text>
+            <Text>Typescript</Text>
+            <Text>WebGL</Text>
+          </div>
+        </div>
+        <div style={{ width: "60%" }}>
+          <h2 style={{ fontSize: "28px" }}>Best of Both Worlds</h2>
+          <Text style={{ marginBottom: "50px" }}>
+            I was fortunate enough to have been exposed and trained for both
+            design and development. I've utilised these skills to help create
+            functional, pleasing and visually appealing websites and
+            applications. Knowing the ups and downs of both sides also make it a
+            more efficient process of figuring what works and what doesn't for
+            our current goal at hand.
+          </Text>
+          <h2 style={{ fontSize: "28px" }}>
+            Iterative and Human-Centered Approach
+          </h2>
+          <Text style={{ marginBottom: "50px" }}>
+            Ever since being exposed to research, design sprints and design
+            thinking, I've absorbed much of their processes and have
+            incorporated it into my current methodology as well. I find that
+            iteration and testing is the cornerstone of delivering a
+            well-rounded product or project.
+          </Text>
+          <h2 style={{ fontSize: "28px" }}>Involved &amp; Feedback Driven</h2>
+          <Text style={{ marginBottom: "50px" }}>
+            This philosophy of design has greatly improved how I go about
+            helping clients achieve their goals. I get quite picky with my
+            clientelle as I prefer to have an open and immersive feedback
+            channel when working on projects. I firmly believe that if the
+            client can openly communicate their ideas and are open to
+            suggestions and improvements to their proposals, there's no other
+            way for the project to go but up.
+          </Text>
+          <h2 style={{ fontSize: "28px" }}>
+            Sounds Like a Fit for Your Team or Project?
+          </h2>
+          <Text style={{ marginBottom: "50px" }}>
+            If you’re curious as to how I work and would like collaborate, pitch
+            a project, know more about my process working with clients, or even
+            just grab a coffee somewhere; please feel free to drop me an e-mail
+            at{" "}
+            <a href="mailto:robbie.hilado@gmail.com">robbie.hilado@gmail.com</a>
+            .
+          </Text>
+        </div>
+      </div>
+    </div>
+  </Layout>
+)
+
+export default AboutPage
