f200febab25367c4061583dafbd115de58836194
sopasdigital <rob.hilado@gmail.com>
2020-07-10 16:03:28 +0100

    changed the image loading behaviour of gatsby image

--- src/components/image.js
+++ src/components/image.js
@@ -19,7 +19,7 @@ const Image = () => {
       placeholderImage: file(relativePath: { eq: "logo.png" }) {
         childImageSharp {
           fluid(maxWidth: 300) {
-            ...GatsbyImageSharpFluid
+            ...GatsbyImageSharpFluid_noBase64
           }
         }
       }
